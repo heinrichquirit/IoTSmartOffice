c54f516a656a202c4550827a3569f5892454539b
Martin Tran <martinlfc95@gmail.com>
2018-10-04 14:44:24 +1000

    update

--- test website/hello.py
+++ test website/hello.py
@@ -1,4 +1,5 @@
 from flask import Flask, url_for, redirect, render_template
+import os
 app = Flask(__name__)
 
 @app.route('/')
@@ -7,11 +8,10 @@ def index():
     name = "Home"
     return render_template('index.html', title=title, name=name)
 
-@app.route('/doctor')
+@app.route("/doctor.html")
 def doctor():
-    title = "Doctor"
-    name = "Doctor"
-    return redirect(url_for('doctor.html'), title=title, name=name)
+    return render_template('doctor.html')
 
 if __name__ == "__main__":
-    app.run()
+    host = os.popen('hostname -I').read()
+    app.run(host='0.0.0.0', port=80, debug=False)
