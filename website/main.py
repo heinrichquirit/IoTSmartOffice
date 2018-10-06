0bc51bc543e607573d1a1466c05fe25ccdede114
Martin Tran <martinlfc95@gmail.com>
2018-10-06 13:29:30 +1000

    finished linking pages

--- website/main.py
+++ website/main.py
@@ -1,15 +1,7 @@
-<<<<<<< HEAD:test website/main.py
 from flask import Flask, render_template, flash, request
 from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
 import os
 
-=======
-import sys
-sys.path.insert(0, 'IoTSmartOffice/Python/cloud_api.py')
-
-import cloud_api
-from flask import Flask, url_for, redirect, render_template
->>>>>>> cf4ec0f2fb205663cf8422210781a48166a3fa6d:website/main.py
 app = Flask(__name__)
 
 class ReusableForm(Form):
@@ -35,11 +27,41 @@ def doctor():
     name = 'Doctor'
     return render_template('doctor.html', title=title, name=name)
 
+@app.route('/doc_view_pats.html')
+def doc_view_pats():
+	title = 'View Patients'
+	name = 'View Patients'
+	return render_template('doc_view_pats.html', title=title, name=name)
+
+@app.route('/doc_add_avail.html')
+def doc_add_avail():
+	title = 'Add Availability'
+	name = 'Add Availability'
+	return render_template('doc_add_avail.html', title=title, name=name)
+
 @app.route('/clerk.html')
 def clerk():
 	title = 'Clerk'
 	name = 'Clerk'
 	return render_template('clerk.html', title=title, name=name)
+
+@app.route('/clerk_view_app.html')
+def clerk_view_app():
+	title = 'View all appointments'
+	name = 'View all appointments'
+	return render_template('clerk_view_app.html', title=title, name=name)
+
+@app.route('/clerk_add_app.html')
+def clerk_add_app():
+	title = 'Add appointment'
+	name = 'Add appointment'
+	return render_template('clerk_add_app.html', title=title, name=name)
+
+@app.route('/clerk_cancel_app.html')
+def clerk_cancel_app():
+	title = 'Cancel appointment'
+	name = 'Cancel appointment'
+	return render_template('clerk_cancel_app.html', title=title, name=name)
 
 @app.route('/patient.html')
 def patient():
@@ -47,7 +69,6 @@ def patient():
 	name = 'Patient'
 	return render_template('patient.html', title=title, name=name)
 
-<<<<<<< HEAD:test website/main.py
 @app.route("/pat_register.html", methods=['GET', 'POST'])
 def hello():
     form = ReusableForm(request.form)
@@ -67,11 +88,18 @@ def hello():
  
     return render_template('pat_register.html', form=form)
 
+@app.route('/pat_add_app.html')
+def pat_add_app():
+	title = 'Make Appointment'
+	name = 'Make Appointment'
+	return render_template('pat_add_app.html', title=title, name=name)
+
+@app.route('/pat_cancel_app.html')
+def pat_cancel_app():
+	title = 'Cancel Appointment'
+	name = 'Cancel Appointment'
+	return render_template('pat_cancel_app.html', title=title, name=name)
+
 if __name__ == "__main__":
 	host = os.popen('hostname -I').read()
 	app.run(host=host, port=80, debug=False)
-=======
-if __name__ == '__main__':
-	connect_to_database()
-	app.run()
->>>>>>> cf4ec0f2fb205663cf8422210781a48166a3fa6d:website/main.py
